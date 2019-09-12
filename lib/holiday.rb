4a0033a2aee71e55b787d454d6d648def6085372
Anjana Nair <anju740@gmail.com>
2019-09-12 04:14:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,7 @@ def all_supplies_in_holidays(holiday_hash)
         attribute = attribute.split.map(&:capitalize).join(' ')
       end
       puts "#{season.to_s.capitalize!}:"
-      puts "  #{attribute.to_s.titleize}: #{value.join(", ")}"
+      puts "  #{attribute.to_s.split("_").map(&:capitalize).join(' ')}: #{value.join(", ")}"
     end
   end
 end
