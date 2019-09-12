48ad667324a3a0063fa17ba1258dc65ed3b2ea9e
Anjana Nair <anju740@gmail.com>
2019-09-12 03:49:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, value|
+      attribute.to_s
       if attribute.to_s.include?("_")
         attribute.to_s.split("_")
         attribute.to_s.capitalize!
